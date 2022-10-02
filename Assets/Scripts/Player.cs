5efd5c8f973a6e3ca001a60069ccb6c9b0b731ed
Viktor Holta <viktor.holta@outlook.com>
2022-10-02 09:58:02 +0200

    Create player idle/walk animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour, ITriggerListener
 
     [SerializeField] private TextMeshProUGUI interactionTextUI;
 
+    [SerializeField] private Animator animator;
 	private Controller2D controller;
 
     private void Awake()
@@ -29,10 +30,29 @@ public class Player : MonoBehaviour, ITriggerListener
         {
             float horizontalMoveDir = Input.GetAxisRaw("Horizontal");
             float verticalMoveDir = Input.GetAxisRaw("Vertical");
+            
+            if (horizontalMoveDir == 0 && verticalMoveDir != 0) // Moving horizontally only
+            {
+                animator.SetFloat("horizontalDirection", 0);
+                animator.SetFloat("verticalDirection", verticalMoveDir);
+            }
+            else if (horizontalMoveDir != 0 && verticalMoveDir == 0) // Moving vertically only
+            {
+                animator.SetFloat("horizontalDirection", horizontalMoveDir);
+                animator.SetFloat("verticalDirection", 0);
+            }
+            else if (velocity == Vector2.zero && horizontalMoveDir != 0 && verticalMoveDir != 0) // Starting moving diagonally
+            {
+                animator.SetFloat("horizontalDirection", horizontalMoveDir);
+                animator.SetFloat("verticalDirection", 0);
+            }
 
             velocity = Vector2.right * horizontalMoveDir + Vector2.up * verticalMoveDir;
             velocity.Normalize();
             controller.Move(velocity * moveSpeed * Time.deltaTime);
+        
+            animator.SetFloat("velocity", velocity.magnitude);
+            animator.SetBool("isWalking", velocity != Vector2.zero);
         }
     }
 
