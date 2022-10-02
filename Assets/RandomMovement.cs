016164af434eb564312de7af286b85ee3930d3b1
Isak Horvath <isakhorvath@gmail.com>
2022-10-02 16:51:01 +0200

    FIX: Start work on visual improvements

---
+++ Assets/RandomMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    [SerializeField]
+    float offset;
+    
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    private Vector2 minPos, maxPos;
+
+    Vector2 target;
+    Vector2 startPos;
+
+    void GetRandomStartPosition() {
+        transform.position = new Vector2(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y));
+    }
+
+    void GetNewTargetPosition() {
+        float randomX = Random.Range(startPos.x - offset, startPos.x + offset);
+        float randomY = Random.Range(startPos.y - offset, startPos.y + offset);
+        target = new Vector2(randomX, randomY);
+    }
+
+    void Start() {
+        GetRandomStartPosition();
+        startPos = transform.position; 
+        GetNewTargetPosition();
+    }
+
+    void Update() {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+        if(Vector2.Distance(transform.position, target) <= 0.01f) {
+            GetNewTargetPosition();
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube((maxPos + minPos) / 2, maxPos - minPos + Vector2.one * 2);
+    }
+}
---
+++ Assets/TitleScreenGenerator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenGenerator : MonoBehaviour
+{
+    [SerializeField] private int animalAmount;
+    [SerializeField] private int decorationAmount;
+
+    [SerializeField] private List<GameObject> decorations = new List<GameObject>();
+    [SerializeField] private GameObject animalPrefab;
+
+    void Start() {
+        for(int i = 0; i < animalAmount; i++)
+            Instantiate(animalPrefab);
+        for(int i = 0; i < decorationAmount; i++)
+            Instantiate(decorations[Random.Range(0, decorations.Count)]);
+    }
+}
